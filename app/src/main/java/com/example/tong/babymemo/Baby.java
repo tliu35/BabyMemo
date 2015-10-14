c85c07ed285785b22ed7c10ea7f0b99d281c7ab0
tliu35@uic.edu <Tong227210>
2015-10-14 01:25:43 -0500

    complete the Baby class

--- app/src/main/java/com/example/tong/babymemo/Baby.java
+++ app/src/main/java/com/example/tong/babymemo/Baby.java
@@ -2,11 +2,34 @@ package com.example.tong.babymemo;
 
 import java.util.Date;
 
-/**
- * Created by Tong on 9/28/15.
- */
 public class Baby {
     private static String name = "Baby";    //if the user does not enter a name, then use Baby as default name
     private static String gender;
     private static Date birthday;
+
+    public static String getName() {
+        return name;
+    }
+
+    public static String getGender() {
+        return gender;
+    }
+
+    public static void setGender(String gender) {
+        Baby.gender = gender;
+    }
+
+    public static void setName(String name) {
+        Baby.name = name;
+    }
+
+    public static Date getBirthday() {
+        return birthday;
+    }
+
+    public static void setBirthday(Date birthday) {
+        Baby.birthday = birthday;
+    }
+
+
 }
